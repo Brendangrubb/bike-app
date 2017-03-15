694b70efdbfbc283d2e65e47149d743908a2be08
ash laidlaw and brendan grubb <brendan@>
2017-03-15 16:55:06 -0700

    add styling and other cool stuff, why are you reading my commit history anyway?

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -6,7 +6,15 @@ $(function() {
   $("#search-brand").click(function() {
     $("#results").empty();
     var brand = $("#brand").val().toString();
-    $("#brand").val("");
-    BikeObject.getBikefromBrand(brand);
+    var city = $("#city").val().toString();
+    var date_stolen = $("#date-stolen").val();
+    console.log(date_stolen);
+    var date_stolen_unix = (new Date(date_stolen).getTime() / 1000).toFixed(0);
+    console.log(date_stolen_unix);
+    BikeObject.getBikes(brand, city, date_stolen_unix);
   });
+});
+
+$(document).ready(function(){
+  setInterval(function() { $('#time').text(moment().format("X")); } , 1000);
 });
--- js/bike.js
+++ js/bike.js
@@ -1,28 +1,29 @@
 function Bike() {}
 
-Bike.prototype.getBikefromBrand = function(brand) {
-  $.get("https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=" + brand + "&location=Portland%2C%20OR&distance=100&stolenness=proximity").then(function(response) {
+Bike.prototype.getBikes = function(brand, city, date_stolen_unix) {
+  $.get("https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=" + brand + "&location=" + city + "%2C%20OR&distance=100&stolenness=proximity").then(function(response) {
     for (var i = 0; i < response.bikes.length; i++) {
       $("#results-table").show();
 
       var img_result;
 
       if (response.bikes[i].thumb === null) {
-        img_result = "No image available"
+        img_result = "No image available";
       } else {
         img_result = "<img src='" + response.bikes[i].thumb + "' alt='A stolen " + response.bikes[i].title + "'>";
       }
-
-      $("#results").append(
-        "<tr>" +
-          "<td>" + response.bikes[i].manufacturer_name + "</td>" +
-          "<td>" + response.bikes[i].title + "</td>" +
-          "<td>" + response.bikes[i].serial + "</td>" +
-          "<td>" + response.bikes[i].id + "</td>" +
-          "<td>" + response.bikes[i].year + "</td>" +
-          "<td>" + img_result + "</td>" +
-        "</tr>"
-      );
+      if (date_stolen_unix >= response.bikes[i].date_stolen) {
+        $("#results").append(
+          "<tr>" +
+            "<td>" + response.bikes[i].manufacturer_name + "</td>" +
+            "<td>" + response.bikes[i].title + "</td>" +
+            "<td>" + response.bikes[i].serial + "</td>" +
+            "<td>" + response.bikes[i].id + "</td>" +
+            "<td>" + response.bikes[i].year + "</td>" +
+            "<td>" + img_result + "</td>" +
+          "</tr>"
+        );
+      }
     }
   });
 };
