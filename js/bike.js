b0f5950354a302e02621ece508942afc7f635799
ash laidlaw and brendan grubb <brendan@>
2017-03-15 13:16:23 -0700

    get bike ids from api to print on page

--- js/bike.js
+++ js/bike.js
@@ -8,9 +8,11 @@ function Bike() {}
 // };
 
 Bike.prototype.getBikefromBrand = function(brand) {
-  $.get("https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=" + brand + "&location=Portland%2C%20OR&distance=100&stolenness=proximity", function(response) {
-    console.log(response);
-    // $("#results").text("Your results: " + response.bikes.serial);
+  $.get("https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=" + brand + "&location=Portland%2C%20OR&distance=100&stolenness=proximity").then(function(response) {
+    console.log(response.bikes);
+    for (var i = 0; i < response.bikes.length; i++) {
+      $("#results").append("<li>" + response.bikes[i].id + "</li>");
+    }
   });
 };
 
