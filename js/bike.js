0faf58980e35239a6ba6a06dee32c7b5f8128336
ash laidlaw and brendan grubb <ash@>
2017-03-15 15:16:50 -0700

    Add image null copy

--- js/bike.js
+++ js/bike.js
@@ -1,24 +1,28 @@
 function Bike() {}
 
-// Bike.prototype.getBikefromSerial = function(serial) {
-//   $.get("https://bikeindex.org:443/api/v3/search?page=1&per_page=25&serial=" + serial + "&location=Portland%2C%20OR&distance=10&stolenness=proximity", function(response) {
-//     console.log(response);
-//     // $("#results").text("Your results: " + response.bikes.serial);
-//   });
-// };
-
 Bike.prototype.getBikefromBrand = function(brand) {
   $.get("https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=" + brand + "&location=Portland%2C%20OR&distance=100&stolenness=proximity").then(function(response) {
     for (var i = 0; i < response.bikes.length; i++) {
       $("#results-table").show();
-      $("#results").append("<tr>" +
+
+      var img_result;
+
+      if (response.bikes[i].thumb === null) {
+        img_result = "No image available"
+      } else {
+        img_result = "<img src='" + response.bikes[i].thumb + "' alt='A stolen " + response.bikes[i].title + "'>";
+      }
+
+      $("#results").append(
+        "<tr>" +
           "<td>" + response.bikes[i].manufacturer_name + "</td>" +
           "<td>" + response.bikes[i].title + "</td>" +
           "<td>" + response.bikes[i].serial + "</td>" +
           "<td>" + response.bikes[i].id + "</td>" +
           "<td>" + response.bikes[i].year + "</td>" +
-          "<td><img src='" + response.bikes[i].thumb + "' alt='A stolen " + response.bikes[i].manufacturer_name + "'</td>" +
-        "</tr>");
+          "<td>" + img_result + "</td>" +
+        "</tr>"
+      );
     }
   });
 };
