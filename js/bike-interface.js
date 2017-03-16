d09c124ca394f00e7f4ff733f718c756e36fcd55
ash laidlaw and brendan grubb <brendan@>
2017-03-16 14:27:59 -0700

    Refactor back-end into front-end

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,10 +1,42 @@
 var Bike = require("./../js/bike.js").bikeModule;
 
+function displayBikes(city, bikeData, user_date_unix_1, user_date_unix_2) {
+  for (var i = 0; i < bikeData.bikes.length; i++) {
+    var img_result;
+    var date_stolen = moment.unix(bikeData.bikes[i].date_stolen).format("MM/DD/YYYY");
+    var location = bikeData.bikes[i].stolen_location.split(",");
+    var location_join = location.join(", ");
+
+    $("#results-table").show();
+
+    if (bikeData.bikes[i].thumb === null) {
+      img_result = "No image available";
+    } else {
+      img_result = "<img src='" + bikeData.bikes[i].thumb + "' alt='A stolen " + bikeData.bikes[i].title + "'>";
+    }
+
+    if ((user_date_unix_1 <= bikeData.bikes[i].date_stolen) && (user_date_unix_2 >= bikeData.bikes[i].date_stolen)) {
+      $("#results").append(
+        "<tr>" +
+          "<td>" + bikeData.bikes[i].manufacturer_name + "</td>" +
+          "<td>" + bikeData.bikes[i].title + "</td>" +
+          "<td>" + bikeData.bikes[i].serial + "</td>" +
+          "<td>" + bikeData.bikes[i].year + "</td>" +
+          "<td>" + location_join + "</td>" +
+          "<td>" + date_stolen + "</td>" +
+          "<td>" + img_result + "</td>" +
+        "</tr>"
+      );
+    }
+  }
+}
+
 $(function() {
   var BikeObject = new Bike();
 
   $("#search-brand").click(function() {
     $("#results").empty();
+
     var brand = $("#brand").val().toString();
     var city = $("#city").val().toString();
     var user_date_1 = $("#date-stolen-1").val();
@@ -12,10 +44,6 @@ $(function() {
     var user_date_unix_1 = (new Date(user_date_1).getTime() / 1000).toFixed(0);
     var user_date_unix_2 = (new Date(user_date_2).getTime() / 1000).toFixed(0);
 
-    BikeObject.getBikes(brand, city, user_date_unix_1, user_date_unix_2);
+    BikeObject.getBikes(brand, city, user_date_unix_1, user_date_unix_2, displayBikes);
   });
-});
-
-$(document).ready(function(){
-  setInterval(function() { $('#time').text(moment().format("X")); } , 1000);
 });
--- js/bike.js
+++ js/bike.js
@@ -2,37 +2,12 @@ var mapApiKey = require('./../.env').apiModule;
 
 function Bike() {}
 
-Bike.prototype.getBikes = function(brand, city, user_date_unix_1, user_date_unix_2) {
-  $.get("https://bikeindex.org:443/api/v3/search?page=1&per_page=30&manufacturer=" + brand + "&location=" + city + "%2C%20OR&distance=100&stolenness=proximity").then(function(response) {
-    for (var i = 0; i < response.bikes.length; i++) {
-      $("#results-table").show();
-
-      var img_result;
-
-      if (response.bikes[i].thumb === null) {
-        img_result = "No image available";
-      } else {
-        img_result = "<img src='" + response.bikes[i].thumb + "' alt='A stolen " + response.bikes[i].title + "'>";
-      }
-
-      var date_stolen = moment.unix(response.bikes[i].date_stolen).format("MM/DD/YYYY");
-      var location = response.bikes[i].stolen_location.split(",");
-      var location_join = location.join(", ");
-
-      if ((user_date_unix_1 <= response.bikes[i].date_stolen) && (user_date_unix_2 >= response.bikes[i].date_stolen)) {
-        $("#results").append(
-          "<tr>" +
-            "<td>" + response.bikes[i].manufacturer_name + "</td>" +
-            "<td>" + response.bikes[i].title + "</td>" +
-            "<td>" + response.bikes[i].serial + "</td>" +
-            "<td>" + response.bikes[i].year + "</td>" +
-            "<td>" + location_join + "</td>" +
-            "<td>" + date_stolen + "</td>" +
-            "<td>" + img_result + "</td>" +
-          "</tr>"
-        );
-      }
-    }
+Bike.prototype.getBikes = function(brand, city, user_date_unix_1, user_date_unix_2, displayBikes)
+{
+  $.get("https://bikeindex.org:443/api/v3/search?page=1&per_page=30&manufacturer=" + brand + "&location=" + city + "%2C%20OR&distance=100&stolenness=proximity").then(function(response)
+  {
+    displayBikes(city, response, user_date_unix_1, user_date_unix_2);
   });
 };
+
 exports.bikeModule = Bike;
